22e633ce6ae4bb3e3e446a263cffc7b361787226
James Eissah Aidoo <jamesaidooessah@gmail.com>
2021-04-16 17:35:23 +0000

    Add json fixtures and serialization library.

--- lib/features/numbers_trivia/data/models/number_trivia_model.dart
+++ lib/features/numbers_trivia/data/models/number_trivia_model.dart
@@ -1,8 +1,17 @@
 import 'package:flutter_clean_architecture/features/numbers_trivia/domain/models/number_trivia.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'NumberTriviaModel.g.dart';
+
+@JsonSerializable()
 class NumberTriviaModel extends NumberTrivia {
   NumberTriviaModel({
     required int number,
     required String text,
   }) : super(number: number, text: text);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) =>
+      _$NumberTriviaModel.fromJson();
+
+  Map<String, dynamic> toJson() => _$NumberTriviaModel.toJson(this);
 }
